a154f5aaa6c3d16fc31855b12b1a0a1999634b76
John Angel <usw4763@gmail.com>
2022-05-08 15:24:46 -0500

    Spinner component 📦
    Fixing spinner component for withoutAuthOnly hoc
    Updating background color of the spinner component

--- src/components/misc/Spinner/index.tsx
+++ src/components/misc/Spinner/index.tsx
@@ -2,7 +2,7 @@ import { Flex } from '@chakra-ui/react'
 import styles from './spinner.module.css'
 
 export default () => (
-  <Flex alignItems='center' justifyContent='center' h='full'>
+  <Flex alignItems='center' justifyContent='center' h='full' bg='gray.50'>
     <div className={styles.spinner} />
   </Flex>
 )
