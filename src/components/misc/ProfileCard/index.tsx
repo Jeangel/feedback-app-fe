33edf4b6535977050a73aedfd5b00c2aae1ed347
John Angel <usw4763@gmail.com>
2022-05-08 14:52:30 -0500

    ProfileCard component 📦
    Adding content to ProfileCard component
    Updating NavbarDrawer to use this component

--- src/components/misc/ProfileCard/index.tsx
+++ src/components/misc/ProfileCard/index.tsx
@@ -1,18 +1,53 @@
-import { Box, Button } from '@chakra-ui/react'
-import { signOut } from 'next-auth/react'
+import { Flex, IconButton, Heading, Text, Icon, HStack } from '@chakra-ui/react'
+import { signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { FiLogOut } from 'react-icons/fi'
+import { BsGear } from 'react-icons/bs'
 
 const ProfileCard = () => {
   const router = useRouter()
+  const { data: sessionData } = useSession()
   const handleOnLogout = async () => {
     await signOut()
     router.push('/login')
   }
-
   return (
-    <Box h='178px' w='full' minW='223px' bg='white' borderRadius='10px' p='10px'>
-      <Button onClick={handleOnLogout}>Logout</Button>
-    </Box>
+    <Flex
+      h='178px'
+      w='full'
+      minW='223px'
+      bg='primary-gradient.500'
+      borderRadius='10px'
+      p='24px'
+      direction='column'
+      justifyContent='space-between'
+    >
+      <Flex direction='column' justifyContent='flex-end'>
+        <Heading color='white'>{sessionData?.user.fullName}</Heading>
+        <Text variant='body2' color='white'>
+          @{sessionData?.user.username}
+        </Text>
+      </Flex>
+      <HStack spacing='10px' justifyContent='flex-end'>
+        <IconButton
+          size='xs'
+          variant='unstyled'
+          aria-label='Settings'
+          icon={<Icon as={BsGear} color='white' fontSize='16px' />}
+          alignSelf='flex-end'
+          pb='0'
+        />
+        <IconButton
+          size='xs'
+          variant='unstyled'
+          aria-label='Logout'
+          icon={<Icon as={FiLogOut} color='white' fontSize='16px' />}
+          alignSelf='flex-end'
+          pb='0'
+          onClick={handleOnLogout}
+        />
+      </HStack>
+    </Flex>
   )
 }
 
--- src/components/misc/RoadmapCard/index.tsx
+++ src/components/misc/RoadmapCard/index.tsx
@@ -3,9 +3,9 @@ import { useRouter } from 'next/router'
 import RoadmapLine from './RoadmapLine'
 
 interface IRoadmapCardProps {
-  planned: number,
-  inProgress: number,
-  live: number,
+  planned: number
+  inProgress: number
+  live: number
 }
 
 const RoadmapCard = ({ planned, inProgress, live }: IRoadmapCardProps) => {
@@ -15,17 +15,19 @@ const RoadmapCard = ({ planned, inProgress, live }: IRoadmapCardProps) => {
   }
 
   return (
-    <Box h='178px' w='full' bg='white' borderRadius='10px' p="24px">
-      <Flex direction="row" justify="space-between" marginBottom="20px">
-        <Heading variant="h3">Roadmap</Heading>
-        <Button variant="link" textDecoration="underline" onClick={redirect}>
-          <Text variant="body3" color="secondary.500">View</Text>
+    <Box h='178px' w='full' minW='223px' bg='white' borderRadius='10px' p='24px'>
+      <Flex direction='row' justify='space-between' marginBottom='20px'>
+        <Heading variant='h3'>Roadmap</Heading>
+        <Button variant='link' textDecoration='underline' onClick={redirect}>
+          <Text variant='body3' color='secondary.500'>
+            View
+          </Text>
         </Button>
       </Flex>
-      <Flex direction="column">
-        <RoadmapLine bulletColor="#F49F85" concept="Planned" amount={planned}/>
-        <RoadmapLine bulletColor="#AD1FEA" concept="In-Progress" amount={inProgress}/>
-        <RoadmapLine bulletColor="#62BCFA" concept="Live" amount={live}/>
+      <Flex direction='column'>
+        <RoadmapLine bulletColor='#F49F85' concept='Planned' amount={planned} />
+        <RoadmapLine bulletColor='#AD1FEA' concept='In-Progress' amount={inProgress} />
+        <RoadmapLine bulletColor='#62BCFA' concept='Live' amount={live} />
       </Flex>
     </Box>
   )
