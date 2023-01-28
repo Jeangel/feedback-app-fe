9dda3003e1c27d906b6b583248927d32fa45aba4
John Angel <usw4763@gmail.com>
2023-01-28 11:34:34 +0100

    Fix types issues 🛠️

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -1,10 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/ban-types
 export const isFunction = (fn: any): fn is Function => {
   return fn && typeof fn === 'function'
 }
-export const isNumber = (number: any): number is Number => {
+export const isNumber = (number: any): number is number => {
   return number && typeof number === 'number' && !isNaN(number)
 }
-export const isString = (str: any): str is String => {
+export const isString = (str: any): str is string => {
   return str && typeof str === 'string'
 }
 export const isNil = (param: any): param is null | undefined => {
