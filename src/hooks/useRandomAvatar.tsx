34b347e0e0c2de4d6a116caabac034f59f5ceb91
John Angel <usw4763@gmail.com>
2024-01-08 12:44:16 +0100

    Fix API endpoint to generate avatars

--- src/hooks/useRandomAvatar.tsx
+++ src/hooks/useRandomAvatar.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 
-const API_URL = 'https://avatars.dicebear.com/api/bottts'
+const API_URL = 'https://api.dicebear.com/7.x/bottts/svg'
 
 export const useRandomAvatar = () => {
   const [avatar, setAvatar] = useState<string>('')
   const shuffle = () => {
     const seed = new Date().getUTCMilliseconds()
-    setAvatar(`${API_URL}/${seed}.svg`)
+    setAvatar(`${API_URL}?seed=${seed}`)
   }
   useEffect(() => {
     shuffle()
