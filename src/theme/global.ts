0c0466b5dacddc2044d82979b15358b011d76fa9
John Angel <usw4763@gmail.com>
2024-01-19 19:51:15 +0100

    Fix styles for large devices

--- src/theme/global.ts
+++ src/theme/global.ts
@@ -3,6 +3,12 @@ import { CSSObject } from '@chakra-ui/react'
 const globalStyles: Record<string, CSSObject> = {
   'html, body, #__next': {
     height: '100%',
+    width: '100%',
+    backgroundColor: 'gray.50',
+  },
+  '#__next': {
+    display: 'flex',
+    justifyContent: 'center',
   },
 }
 
